b2adc981de1f349715bc472d6d61452fcab86883
Oishi Yuikyo <s2008511@gmail.com>
2019-01-26 20:52:21 +0900

    Create room_image table

---
+++ db/migrate/20190126113824_create_room_images.rb
@@ -0,0 +1,10 @@
+class CreateRoomImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :room_images do |t|
+      t.text :image
+      t.references :room, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
