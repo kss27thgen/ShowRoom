291ab89da4389ca3c0a78d37486571dbbb3b85e0
Oishi Yuikyo <s2008511@gmail.com>
2019-01-26 23:33:51 +0900

    Edit room post

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,23 @@
 class RoomsController < ApplicationController
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path, notice:"商品を出品しました"
+    else
+      render :purchase, alert:"商品が出品できませんでした。"
+    end
+  end
+
+
+
+
+private
+  def room_params
+    params.require(:room).permit(:name, :since, :area, :agent, :zipcode, :address, :description, :facility, :category, :room_type, :rent, :ward)
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
   has_many :room_images, dependent: :destroy
 
+  enum category: [:戸建て, :マンション, :オフィス]
+  enum room_type: [:"1R", :"1K"]
+  enum ward: [:"目黒", :"品川", "港", "新宿", "渋谷", "世田谷", "杉並", "文京", "千代田", "江東", "中央", "豊島", "台東", "荒川", "葛飾", "足立"]
+
 end
